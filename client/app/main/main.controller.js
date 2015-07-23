3e56223a5a0c06906d6159290fc8cd27f20c9f1d
unkec1986 <unkec@outlook.com>
2015-07-23 05:36:13 +0200

    changes to home and signup page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,22 +1,28 @@
 'use strict';
 
 angular.module('votingApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+    .controller('MainCtrl', function ($scope, $http, $location) {
+        $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+        $http.get('/api/things').success(function (awesomeThings) {
+            $scope.awesomeThings = awesomeThings;
+        });
+
+        $scope.addThing = function () {
+            if ($scope.newThing === '') {
+                return;
+            }
+            $http.post('/api/things', {
+                name: $scope.newThing
+            });
+            $scope.newThing = '';
+        };
+
+        $scope.deleteThing = function (thing) {
+            $http.delete('/api/things/' + thing._id);
+        };
+
+        $scope.signup = function () {
+            $location.path('/signup');
+        }
     });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-  });
